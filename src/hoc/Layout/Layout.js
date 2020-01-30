60f69faecd2f77f029049df731b0a50b65f6f496
ign0ramus <yanushkevich_dmitry@mail.ru>
2020-01-30 14:01:30 +0300

    fix main layout

--- src/hoc/Layout/Layout.js
+++ src/hoc/Layout/Layout.js
@@ -1,6 +1,5 @@
 import React, { Component } from 'react';
 
-import Aux from '../Aux/Aux';
 import classes from './Layout.css';
 import Toolbar from '../../components/Navigation/Toolbar/Toolbar';
 import SideDrawer from '../../components/Navigation/SideDrawer/SideDrawer';
@@ -22,14 +21,14 @@ class Layout extends Component {
 
 	render() {
 		return (
-			<Aux>
+			<div className={classes.Layout}>
 				<Toolbar drawerToggleClicked={this.sideDrawerToggleHandler} />
 				<SideDrawer
 					open={this.state.showSideDrawer}
 					closed={this.sideDrawerClosedHandler}
 				/>
 				<main className={classes.Content}>{this.props.children}</main>
-			</Aux>
+			</div>
 		);
 	}
 }
