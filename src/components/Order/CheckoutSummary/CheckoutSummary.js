892f3383395a29c5d5750b9ea19ef9fb42553aa2
ign0ramus <yanushkevich_dmitry@mail.ru>
2020-02-02 16:08:41 +0300

    refacotr orders components

--- src/components/Order/CheckoutSummary/CheckoutSummary.js
+++ src/components/Order/CheckoutSummary/CheckoutSummary.js
@@ -1,24 +1,31 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 
 import Burger from '../../Burger/Burger';
 import Button from '../../UI/Button/Button';
 import classes from './CheckoutSummary.css';
 
-const checkoutSummary = (props) => {
-    return (
-        <div className={classes.CheckoutSummary}>
-            <h1>We hope it tastes well!</h1>
-            <div style={{width: '100%', margin: 'auto'}}>
-                <Burger ingredients={props.ingredients}/>
-            </div>
-            <Button 
-                btnType="Danger"
-                clicked={props.checkoutCancelled}>CANCEL</Button>
-            <Button 
-                btnType="Success"
-                clicked={props.checkoutContinued}>CONTINUE</Button>
-        </div>
-    );
-}
+const CheckoutSummary = props => {
+	return (
+		<div className={classes.CheckoutSummary}>
+			<h1>We hope it tastes well!</h1>
+			<div style={{ width: '100%', margin: 'auto' }}>
+				<Burger ingredients={props.ingredients} />
+			</div>
+			<Button btnType='Danger' onClick={props.checkoutCancelled}>
+				CANCEL
+			</Button>
+			<Button btnType='Success' onClick={props.checkoutContinued}>
+				CONTINUE
+			</Button>
+		</div>
+	);
+};
 
-export default checkoutSummary;
+CheckoutSummary.propTypes = {
+	ingredients: PropTypes.object.isRequired,
+	checkoutCancelled: PropTypes.func.isRequired,
+	checkoutContinued: PropTypes.func.isRequired,
+};
+
+export default CheckoutSummary;
