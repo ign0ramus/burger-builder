0d14d628cc41d77a0bf28c9adb2b35f4ab52dbc7
ign0ramus <yanushkevich_dmitry@mail.ru>
2020-02-07 23:28:15 +0300

    refactor burgerBuilder saga for tests

--- src/redux/sagas/burgerBuilder.js
+++ src/redux/sagas/burgerBuilder.js
@@ -1,12 +1,11 @@
-import { put } from 'redux-saga/effects';
+import { put, call } from 'redux-saga/effects';
 
 import axios from '../../axios-orders';
 import { setIngredients, fetchIngredientsFailed } from '../actions';
 
 export function* getIngredientsSaga(action) {
 	try {
-		const res = yield axios.get('/ingredients.json');
-
+		const res = yield call([axios, 'get'], '/ingredients.json');
 		yield put(setIngredients(res.data));
 	} catch (err) {
 		yield put(fetchIngredientsFailed());
